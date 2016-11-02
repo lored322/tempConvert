c7268ca32bc364e09e2e43b4312639954b90e2ca
Edana Lord <e.lord223@gmail.com>
2016-11-02 13:54:51 +1300

    add all files to new repo

---
+++ R/fahr_to_Kelvin.R
@@ -0,0 +1,16 @@
+#' Convert Fahrenheit to Kelvin
+#'
+#' This function converts input temperatures in Fahrenheit to Kelvin.
+#' @param temp The input temperature.
+#' @export
+#' @examples
+#' fahr_to_kelvin(32)
+
+fahr_to_kelvin <- function(temp) {
+  #Converts Fahrenheit to Kelvin
+  kelvin <- ((temp - 32) * (5/9)) + 273.15
+  kelvin
+}
+
+fahr_to_kelvin(100)
+?fahr_to_kelvin
